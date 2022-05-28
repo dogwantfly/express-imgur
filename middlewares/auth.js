37147f8f0a97cab98c1f9df302032b1718abe7be
hsin-yu <tina2793778@gmail.com>
2022-05-28 23:51:09 +0800

    移除修復程式碼片段

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -17,7 +17,7 @@ const isAuth = handleErrorAsync(async (req, res, next) => {
   const decoded = await new Promise((resolve, reject) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
-        setTimeout(reject(err), 500)
+        reject(err)
       } else {
         resolve(payload);
       }
